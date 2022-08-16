1f061c0c8786d1d542b2cf18ef091eb3921cc35c
xiang-zheng <sailordurden@gmail.com>
2022-08-16 22:38:48 +1000

    show history list and delete history item

--- src/components/UserHistory/index.js
+++ src/components/UserHistory/index.js
@@ -1,10 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { foodSliceActions } from "../../store/food-slice.js";
+
 import { historySliceActions } from "../../store/history-slice.js";
 
 import * as React from "react";
 import Button from "@mui/material/Button";
+
+// const initialHistoryState = {
+//   history: [
+//     {
+//       historyID: "ab date time",
+//       userName: "ab",
+//       title: "lunch",
+//       dateTimeOfMeal: "datetime",
+//       userInput: "",
+//       apiResult: [], //same as apiOutput in food-slice.js
+//       totalCalory: 0,
+//       timeSaved: "",
+//     },
+//     {
+//       historyID: "0",
+//       userName: "ab",
+//       title: "dinner",
+//       dateTimeOfMeal: "",
+//       userInput: "",
+//       apiResult: [], //same as apiOutput in food-slice.js
+//       totalCalory: 0,
+//       timeSaved: "",
+//     },
+//   ],
+// };
 
 const UserHistory = () => {
   const history = useSelector((state) => state.historySlice.history);
@@ -13,13 +38,41 @@ const UserHistory = () => {
   const dispatch = useDispatch();
   let navigate = useNavigate();
 
+  const currentUserHistory = history.filter(
+    (item) => item.userName === currentUser
+  );
+
+  const handleHistoryDelete = (e, historyIndex) => {
+    e.preventDefault();
+    console.log(historyIndex);
+    dispatch(historySliceActions.deletHistoryByIndex(historyIndex));
+  };
   return (
     <div>
-      <h3>{currentUser} History </h3>
+      <h1>{currentUser}'s Saved History </h1>
+      {currentUserHistory.length === 0 && <p>No saved history</p>}
       <ul>
-        {history.map((item, index) => (
-          <li key={item.historyID}>
-            {item.apiResult[0][0]} serving {item.apiResult[0][1].label}
+        {currentUserHistory.map((item, historyIndex) => (
+          <li>
+            <h2>
+              {item.title} on the date of {item.dateTimeOfMeal}:{" "}
+              {item.totalCalory} KCal
+            </h2>
+
+            <ul>
+              {item.apiResult.map((foodItem, foodItemIndex) => (
+                <li key={foodItem[1].foodId}>
+                  {foodItem[0]} serving {foodItem[1].label}
+                </li>
+              ))}
+            </ul>
+            <button
+              onClick={(e) => {
+                handleHistoryDelete(e, historyIndex);
+              }}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
@@ -28,3 +81,11 @@ const UserHistory = () => {
 };
 
 export default UserHistory;
+
+// <ul>
+// {history.map((item, index) => (
+//   <li key={item.historyID}>
+//     {item.apiResult[0][0]} serving {item.apiResult[0][1].label}
+//   </li>
+// ))}
+// </ul>
--- src/store/history-slice.js
+++ src/store/history-slice.js
@@ -3,6 +3,16 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialHistoryState = {
   history: [
     // {
+    //   historyID: "ab date time",
+    //   userName: "ab",
+    //   title: "",
+    //   dateTimeOfMeal: "",
+    //   userInput: "",
+    //   apiResult: [], //same as apiOutput in food-slice.js
+    //   totalCalory: 0,
+    //   timeSaved: "",
+    // },
+    // {
     //   historyID: "0",
     //   userName: "",
     //   title: "",
@@ -21,8 +31,11 @@ const historySlice = createSlice({
   reducers: {
     addToHistory(state, action) {
       console.log(action.payload);
-      state.history.push(action.payload);
+      state.history.unshift(action.payload);
       console.log(state.history.length);
+    },
+    deletHistoryByIndex(state, action) {
+      state.history.splice(action.payload, 1);
     },
   },
 });
